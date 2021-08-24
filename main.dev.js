cd1c281da06801c66b0c7ab7fb7b0ba7141db839
jeromekithinji <kithinjijerome@gmail.com>
2021-08-24 16:46:03 +0100

    Added the spaces for the letters of the words

--- main.dev.js
+++ main.dev.js
@@ -1,11 +1,11 @@
 "use strict";
 
+console.log("JavaScript works");
+
 function footerText() {
   var footerText = document.getElementById("game__footer");
   footerText.innerHTML = "Jerome Kithinji, ".concat(new Date().getFullYear(), ". ");
-  console.log(footerText.innerHTML);
 }
 
 ;
 footerText();
-console.log("it works");
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
+console.log("JavaScript works");
+
 function footerText() {
     let footerText = document.getElementById("game__footer");
     footerText.innerHTML = `Jerome Kithinji, ${new Date().getFullYear()}. `;
-    console.log(footerText.innerHTML);
 };
 
 footerText();
 
-console.log("it works");
