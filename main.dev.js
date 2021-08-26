28d2a7e2818c2d8e63b3a4606e9845db851fb5a5
jeromekithinji <kithinjijerome@gmail.com>
2021-08-26 09:23:05 +0100

    Inserting word characters from array functionality works

--- main.dev.js
+++ main.dev.js
@@ -8,4 +8,26 @@ function footerText() {
 }
 
 ;
-footerText();
+footerText(); // Array of words 
+
+var words = [["Dog", "Mans bestfriend"], ["Cheetah", "Fastest animal"]]; // Getting random number to pick a word from array
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * words.length);
+}
+
+; // Generates a random number
+// console.log(getRandomNumber());
+// Inserting the word into the blanks on the website by picking the word from the generated random number and looping the li element 
+
+function wordLetters() {
+  word = words[getRandomNumber()][0];
+  console.log(word);
+
+  for (i = 0; i < word.length; i++) {
+    var html = "<li class=\"letter\">".concat(word.charAt(i), "</li>");
+    document.getElementById("game__word").innerHTML += html;
+  }
+}
+
+wordLetters();
--- main.js
+++ main.js
@@ -7,3 +7,31 @@ function footerText() {
 
 footerText();
 
+// Array of words 
+const words = [
+    ["Dog", "Mans bestfriend"], ["Cheetah", "Fastest animal"]
+]
+
+// Getting random number to pick a word from array
+function getRandomNumber() {
+    return Math.floor(Math.random() * words.length);
+};
+
+// Generates a random number
+// console.log(getRandomNumber());
+
+
+// Inserting the word into the blanks on the website by picking the word from the generated random number and looping the li element 
+function wordLetters () {
+    word = (words[getRandomNumber()][0]);
+    console.log(word);
+    for (i = 0; i < word.length; i++) {
+        const html = `<li class="letter">${word.charAt(i)}</li>`;
+        document.getElementById("game__word").innerHTML += html;
+    }
+}
+
+wordLetters();
+
+
+
